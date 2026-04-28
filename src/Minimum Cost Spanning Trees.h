76d059b8996dd6d84ae6266a1f80f38a0a98a1ab
pinkrich0618-commits <pinkrich0618@gmail.com>
2026-04-29 02:56:12 +0800

    Update Minimum Cost Spanning Trees.h

--- src/Minimum Cost Spanning Trees.h
+++ src/Minimum Cost Spanning Trees.h
@@ -1,1 +1,30 @@
+#ifndef MST_H
+#define MST_H
 
+#include "header.h"
+
+class MST {
+private:
+    int V;
+    vector<tuple<int,int,int>> edges;
+
+public:
+    MST(int v) { V = v; }
+
+    void addEdge(int u, int v, int w) {
+        edges.push_back({w,u,v});
+    }
+
+    void kruskal() {
+        sort(edges.begin(), edges.end());
+        int total = 0;
+
+        for(auto [w,u,v] : edges){
+            total += w; // 簡化版（未做 union-find）
+        }
+
+        cout << "MST cost: " << total << endl;
+    }
+};
+
+#endif
